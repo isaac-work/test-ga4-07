0b558d8146a0a2a969e7d221a39e172cc83d28b9
isaac-work <117258243+isaac-work@users.noreply.github.com>
2022-11-07 14:08:03 +0800

    Add routes (#1)
    Co-authored-by: Isaac Huang <isaac.huang@westpharma.com>

--- src/index.js
+++ src/index.js
@@ -1,17 +1,97 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {
+  createRoutesFromElements,
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
+import "./index.css";
+import ErrorPage from "./error-page";
+import Contact, {
+  loader as contactLoader,
+  action as contactAction,
+} from "./routes/contact";
+import Root, {
+  loader as rootLoader,
+  action as rootAction,
+} from "./routes/root";
+import EditContact, {
+  action as editAction,
+} from "./routes/edit";
+import { action as destroyAction } from "./routes/destroy";
+import Index from "./routes/index";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route
+      path="/"
+      element={<Root />}
+      loader={rootLoader}
+      action={rootAction}
+      errorElement={<ErrorPage />}
+    >
+      <Route errorElement={<ErrorPage />}>
+        <Route index element={<Index />} />
+        <Route
+          path="contacts/:contactId"
+          element={<Contact />}
+          loader={contactLoader}
+          action={contactAction}
+        />
+        <Route
+          path="contacts/:contactId/edit"
+          element={<EditContact />}
+          loader={contactLoader}
+          action={editAction}
+        />
+        <Route
+          path="contacts/:contactId/destroy"
+          action={destroyAction}
+        />
+      </Route>
+    </Route>
+  )
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     // element: <div>Hello world!</div>,
+//     element: <Root />,
+//     errorElement: <ErrorPage />,
+//     loader: rootLoader,
+//     action: rootAction,
+//     children: [
+//       {
+//         errorElement: <ErrorPage />,
+//         children: [
+//           { index: true, element: <Index /> },
+//           {
+//             path: "contacts/:contactId",
+//             element: <Contact />,
+//             loader: contactLoader,
+//             action: contactAction,
+//           },
+//           {
+//             path: "contacts/:contactId/edit",
+//             element: <EditContact />,
+//             loader: contactLoader,
+//             action: editAction,
+//           },
+//           {
+//             path: "contacts/:contactId/destroy",
+//             action: destroyAction,
+//             errorElement: <div>Oops! There was an error.</div>,
+//           },
+//         ],
+//       }
+//     ],
+//   }
+// ]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
